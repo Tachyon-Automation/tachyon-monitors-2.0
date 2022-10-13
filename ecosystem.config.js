49d45ced3fbb0598e903adad65229af996fafde2
aggiekouv <aggiekouvaras@gmail.com>
2022-10-12 23:22:05 -0400

    stuff

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -63,5 +63,12 @@ module.exports = {
             "error_file": "/dev/null",
             "out_file": "/dev/null"
         },
+        {
+            "script": "./AIO Sites/snipesusa2.js",
+            "watch": true,
+            "max_memory_restart": "1000M",
+            "error_file": "/dev/null",
+            "out_file": "/dev/null"
+        },
     ]
 }
