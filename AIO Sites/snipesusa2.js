b18057a3aa9709ca5b1767bdc5619499d1ab4a3a
aggiekouv <aggiekouvaras@gmail.com>
2022-10-13 12:28:44 -0400

    start

--- AIO Sites/snipesusa2.js
+++ AIO Sites/snipesusa2.js
@@ -36,13 +36,18 @@ async function monitor(sku) {
         let headers = {
             'User-Agent': randomUseragent.getRandom(),
             'Poq-App-Identifier': '082463f6-579a-46f1-b9c9-7e2f4e01b873',
-            //'x-px-bypass-reason': 'The%20certificate%20for%20this%20server%20is%20invalid.%20You%20might%20be%20connecting%20to%20a%20server%20that%20is%20pretending%20to%20be%20%E2%80%9Cpx-conf.perimeterx.net%E2%80%9D%20which%20could%20put%20your%20confidential%20information%20at%20risk.',
-            'X-PX-AUTHORIZATION': `3:${v4()}`,        
+            'Poq-App-Version': `${v4()}`,
+            'Poq-Platform': 'iOS',
+            'Poq-Platform-Version': `${v4()}`,
+            'Poq-Device-Model': 'iPhone',
+            'x-px-bypass-reason': `${v4()}`,
+            'x-px-bypass': `${v4()}`,
+            'X-PX-AUTHORIZATION': `3:${v4()}`,     
+            'cookie': `_px3=${v4()};_pxhd=${v4()}`
         }
         let method = 'GET'; //request method
         let req = `https://platform.poq.io/clients/snipes/products?ids=${sku}`//request url
         let set = await helper.requestJson(req, method, proxy, headers) //request function
-        //console.log(set.response.status)
         let body = await set.json
         if (set.response.status == 404) {
             await helper.sleep(product.waittime);
@@ -53,6 +58,7 @@ async function monitor(sku) {
             monitor(sku)
             return
         }
+        console.log(set.response.status)
         //Define body variables
         if (body[0].details.name) {
             let inStock = false
