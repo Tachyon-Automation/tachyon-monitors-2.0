5454f07f943cda7ae331d6d3e77584a147dd0041
aggiekouv <aggiekouvaras@gmail.com>
2022-10-13 22:10:35 -0400

    nord fix

---
+++ AIO Sites/asos.js
@@ -0,0 +1,100 @@
+const helper = require('../x-help/helper');
+const sites = require('../x-help/sites.json');
+const groups = require('../x-help/groups.json');
+const database = require('../x-help/database');
+const discordBot = require('../x-help/discord')
+const Discord = require('discord.js');
+const { v4 } = require('uuid');
+const CHANNEL = '810951064341446687' //channel id
+const site = 'ASOS'; //site name
+const version = `ASOS v1.0` //Site version
+const table = site.toLowerCase();
+discordBot.login();
+let PRODUCTS = {}
+startMonitoring()
+async function startMonitoring() {
+    let SKUList = await database.query(`SELECT * from ${table}`);
+    for (let row of SKUList.rows) {
+        PRODUCTS[row.sku] = {
+            sku: row.sku,
+            waittime: row.waittime,
+            sizes: row.sizes
+        }
+        monitor(row.sku)
+    }
+    console.log(`[${site}] Monitoring all SKUs!`)
+}
+
+async function monitor(sku) {
+    try {
+        let product = PRODUCTS[sku]
+        if (!product)
+            return;
+        let proxy = await helper.getRandomProxy(); //proxy per site
+        let pid = ''
+        try { pid = await sku.split('-').join('%20'); }
+        catch (e) { pid = sku }
+
+        //these headers change per site
+        let headers = {
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1 (compatible; AdsBot-Google-Mobile; +http://www.google.com/mobile/adsbot.html)',
+        }
+        let method = 'GET'; //request method
+        let req = `https://api-asos-com.translate.goog/product/catalogue/v3/products/${sku}?store=COM&abcz=${v4()}&_x_tr_sl=el&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp`//request url
+        let set = await helper.requestJson(req, method, proxy, headers) //request function
+        //console.log(set.response.status)
+        let body = await set.json
+        //Custom error handling
+        if (set.response.status == 404) {
+            console.log(sku)
+            return
+        }
+        if (set.response.status != 200) {
+            monitor(sku)
+            return
+        }
+        //Define body variables
+        if (body.isInStock === true) {
+            let inStock = false;
+            let url = `https://www.asos.com/prd/${sku}#Tachyon`//product url
+            let title = body.name
+            let price = body.price.current.text
+            let image = 'https://content.asos-media.com/-/media/images/asos/logo/icon_svg.svg'
+            try { image = 'https://imageresize.24i.com/?w=300&url=https://' + body.media.images[0].url } catch (e) { } //try set image
+            let stock = 0
+            let sizes = []
+            let query = await database.query(`SELECT * from ${table} where sku='${sku}'`);
+            let oldSizeList = await query.rows[0].sizes
+            let sizeList = []
+            //pars sizes for loop
+            for (variant of body.variants) {
+                if (variant.isInStock !== true)
+                    continue
+                sizes += `${variant.displaySizeText.replace('UK ', '')} UK\n`
+                stock++
+                sizeList.push(variant.displaySizeText);
+                if (!oldSizeList.includes(variant.displaySizeText))
+                    inStock = true;
+            }
+            if (inStock) {
+                console.log(`[time: ${new Date().toISOString()}, product: ${sku}, title: ${title}]`)
+                inStock = false;
+                let sizeright = sizes.split('\n')
+                let sizeleft = sizeright.splice(0, Math.floor(sizeright.length / 2))
+                for (let group of sites[site]) {
+                    await helper.postAIO(url, title, sku, price, image, sizeright, sizeleft, stock, groups[group], site, version)
+                }
+                await database.query(`update ${table} set sizes='${JSON.stringify(sizeList)}' where sku='${sku}'`);
+
+            }
+        }
+        await helper.sleep(product.waittime);
+        monitor(sku);
+        return
+    } catch (e) {
+        //console.log(e)
+        monitor(sku)
+        return
+    }
+}
+helper.discordbot(CHANNEL, PRODUCTS, table, monitor, site)
