796d94befcff64195284c450230221d2c20a431a
Aggie <aggiekouvaras@gmail.com>
2022-10-14 14:29:45 -0400

    start

--- aio.config.js
+++ aio.config.js
@@ -48,6 +48,13 @@ module.exports = {
             "max_memory_restart": "1000M",
             "error_file": "/dev/null",
             "out_file": "/dev/null"
+        },
+        {
+            "script": "./AIO Sites/asos.js",
+            "watch": true,
+            "max_memory_restart": "1000M",
+            "error_file": "/dev/null",
+            "out_file": "/dev/null"
         }
     ]
 }
