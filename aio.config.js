03f2d768d95ea4bc89f5849f8c6ffea86eca6ed5
aggiekouv <aggiekouvaras@gmail.com>
2022-10-15 16:36:43 -0400

    start

--- aio.config.js
+++ aio.config.js
@@ -22,13 +22,6 @@ module.exports = {
             "out_file": "/dev/null"
         },
         {
-            "script": "./AIO Sites/hibbett3.js",
-            "watch": true,
-            "max_memory_restart": "1000M",
-            "error_file": "/dev/null",
-            "out_file": "/dev/null"
-        },
-        {
             "script": "./AIO Sites/sns.js",
             "watch": true,
             "max_memory_restart": "1000M",
--- aio2.config.js
+++ aio2.config.js
@@ -34,6 +34,13 @@ module.exports = {
             "max_memory_restart": "1000M",
             "error_file": "/dev/null",
             "out_file": "/dev/null"
-        }
+        },
+        {
+            "script": "./AIO Sites/hibbett3.js",
+            "watch": true,
+            "max_memory_restart": "1000M",
+            "error_file": "/dev/null",
+            "out_file": "/dev/null"
+        },
     ]
 }
