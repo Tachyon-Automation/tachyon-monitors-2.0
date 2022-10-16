4b70e81e4aa7a8b4e59c75597e771c22ad57e7c9
aggiekouv <aggiekouvaras@gmail.com>
2022-10-16 14:22:51 -0400

    start

--- AIO Sites/snipeseu2.js
+++ AIO Sites/snipeseu2.js
@@ -3,11 +3,12 @@ const sites = require('../x-help/sites.json');
 const groups = require('../x-help/groups.json');
 const database = require('../x-help/database');
 const discordBot = require('../x-help/discord')
+const randomUseragent = require('random-useragent');
 const Discord = require('discord.js');
 const { v4 } = require('uuid');
-const CHANNEL = '1031118814861070357' //channel id
-const site = 'FINISHLINE2'; //site name
-const version = `Finishline v3.0` //Site version
+const CHANNEL = '1031268085560266762' //channel id
+const site = 'SNIPESEU2'; //site name
+const version = `Snipes v2.0` //Site version
 const table = site.toLowerCase();
 discordBot.login();
 let PRODUCTS = {}
@@ -33,16 +34,13 @@ async function monitor(sku) {
         let proxy = 'http://usa.rotating.proxyrack.net:9000'; //proxy per site
         //these headers change per site
         let headers = {
-            'User-Agent': 'Finish Line/2.7.3  (Android 2.7.3; Build/2.7.3)',
-            'welove': 'maltliquor'
-        }
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',        }
         let method = 'GET'; //request method
-        let req = `https://prodmobloy2.finishline.com/api/products/${sku}`//request url
+        let req = `https://www.snipes.com/s/snse-DE-AT/dw/shop/v19_5/products/(${sku})?client_id=cf212f59-94d1-4314-996f-7a11871156f4&cache=${v4()}&locale=de-DE&expand=availability,+prices,+promotions,+variations`//request url
         let set = await helper.requestJson(req, method, proxy, headers) //request function
-        //console.log(set.response.status)
         let body = await set.json
-        //Custom error handling
-        if (body.statusCode == 499) {
+        //console.log(set.response.status)
+        if (set.response.status == 404) {
             await helper.sleep(product.waittime);
             monitor(sku);
             return
@@ -52,36 +50,27 @@ async function monitor(sku) {
             return
         }
         //Define body variables
-        if (body.displayName) {
+        body = body.data[0]
+        if (body.inventory.ats > 0 && body.inventory.orderable == true) {
             let inStock = false
-            let title = body.displayName
-            let stock = 0
+            let url = `https://www.snipes.com/${sku}.html#Tachyon`
+            let title = body.name;
+            let price = body.c_akeneo_wwsprice[0]
+            let image = `https://www.snipes.com/dw/image/v2/BDCB_PRD/on/demandware.static/-/Sites-snse-master-eu/default/dwb94c64eb/${await JSON.parse(body.c_akeneo_images)[0]}.jpg?sw=780&sh=780&sm=fit&sfrm=png`
+            let stock = body.inventory.ats
             let sizes = []
             let query = await database.query(`SELECT * from ${table} where sku='${sku}'`);
             let oldSizeList = await query.rows[0].sizes
             let sizeList = []
-            let styleID = ''
-            let colorID = ''
-            let url = ''
-            let colorDescription = ''
-            let image = ''
-            var price
-            //pars sizes for loop
-            for(product of body.colorWays) {
-                styleID = product.styleId
-                colorID = product.colorId
-                colorDescription = product.colorDescription
-                title = title + ' ' + product.colorDescription
-                price = product.salePriceCents/100
-                image = product.images[0].thumbnailUrl.replace('?$Thumbnail$', '')
-                url = `https://www.finishline.com/store/product/tachyon/${sku}?styleId=${styleID}&colorId=${colorID}#Tachyon`//product url
-            for (variant of product.skus) {
-                if (variant.quantityAvailable > 0) {
-                sizes += `${variant.size} (${variant.quantityAvailable}) - ${variant.skuId}\n`
-                stock += variant.quantityAvailable
-                sizeList.push(variant.skuId);
-                if (!oldSizeList.includes(variant.skuId))
-                    inStock = true;
+            let variants = body.variants
+            //pars sizes for l
+            for (let size of variants) {
+                if (size.orderable === true) {
+                    sizes += `[${size.variation_values.size}](https://www.snipes.com/${size.product_id.trim()}.html#Tachyon) - ${size.product_id.trim()}\n`;
+                    sizeList.push(size.variation_values.size);
+                    if (!oldSizeList.includes(size.variation_values.size)) {
+                        inStock = true;
+                    }
                 }
             }
             if (inStock) {
@@ -93,11 +82,8 @@ async function monitor(sku) {
                     await helper.postAIO(url, title, sku, price, image, sizeright, sizeleft, stock, groups[group], site, version)
                 }
                 await database.query(`update ${table} set sizes='${JSON.stringify(sizeList)}' where sku='${sku}'`);
-                sizes = []
-                stock = 0
-                title = body.displayName
+
             }
-        }
         }
         await helper.sleep(product.waittime);
         monitor(sku);
