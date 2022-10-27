2a5387259012ebb72ee42aa6d3a2637cc345cc5d
aggiekouv <aggiekouvaras@gmail.com>
2022-10-27 03:55:27 -0400

    start

---
+++ Retail/wlamart.js
@@ -0,0 +1,96 @@
+
+const helper = require('../x-help/helper');
+const sites = require('../x-help/sites.json');
+const groups = require('../x-help/groups.json');
+const database = require('../x-help/database');
+const discordBot = require('../x-help/discord')
+const randomUseragent = require('random-useragent');
+const HTMLParser = require('node-html-parser');
+const Discord = require('discord.js');
+const { v4 } = require('uuid');
+const CHANNEL = '810935848899444777' //channel id
+const site = 'WALMARTUS'; //site name
+const version = `Walmart US v1.0` //Site version
+const table = site.toLowerCase();
+discordBot.login();
+let PRODUCTS = {}
+startMonitoring()
+async function startMonitoring() {
+    let SKUList = await database.query(`SELECT * from ${table}`);
+    for (let row of SKUList.rows) {
+        PRODUCTS[row.sku] = {
+            sku: row.sku,
+            waittime: row.waittime,
+            sizes: row.sizes
+        }
+        monitor(row.sku)
+    }
+    console.log(`[${site}] Monitoring all SKUs!`)
+}
+
+async function monitor(sku) {
+    try {
+        let product = PRODUCTS[sku]
+        if (!product)
+            return;
+        let proxy = 'http://usa.rotating.proxyrack.net:9000'; //proxy per site
+        //let agent = randomUseragent.getRandom(); //random agent per site
+        //these headers change per site
+        let headers = {
+            'cookie': v4(),
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+        }
+        let method = 'GET'; //request method
+        let req = `https://www.walmart.com/ip/tachyon/${sku}/.js`//request url
+        let set = await helper.requestHtml(req, method, proxy, headers) //request function
+        //console.log(set.response.status)
+        let parse = set.text.split('">{"p')[1].split('scriptLoader":[]}')[0].trim()
+        body = await JSON.parse('{"p' + parse + 'scriptLoader":[]}')
+        let status = product.sizes
+        if (set.response.status == 404) {
+            console.log('Removed - ' + sku)
+            await helper.sleep(product.waittime);
+            monitor(sku);
+            return
+        }
+        if (set.response.status != 200) {
+            monitor(sku);
+            return
+        }
+            if (body.props.pageProps.initialData.data.product.sellerId == 'F55CDC31AB754BB68FE0B39041159D63' && body.props.pageProps.initialData.data.product.shippingOption.availabilityStatus == 'AVAILABLE') {
+                let url = `https://www.walmart.com/ip/tachyon/${sku}#Tachyon`
+                let title = body.props.pageProps.initialData.data.product.name
+                let price = body.props.pageProps.initialData.data.product.priceInfo.currentPrice.priceDisplay
+                let image = body.props.pageProps.initialData.data.product.imageInfo.thumbnailUrl
+                let stock = '1'
+                let offerid = body.props.pageProps.initialData.data.product.offerId
+                if (status !== "In-Stock") {
+                    let qt = 'NA'
+                    let links = `[ATC](http://goto.walmart.com/c/2242082/565706/9383?veh=aff&sourceid=imp_000011112222333344&prodsku=${sku}&u=http%3A%2F%2Faffil.walmart.com%2Fcart%2Fbuynow%3F%3Dveh%3Daff%26affs%3Dsdk%26affsdkversion%3D%26affsdktype%3Djs%26affsdkcomp%3Dbuynowbutton%26colorscheme%3Dorange%26sizescheme%3Dprimary%26affsdkreferer%3Dhttp%253A%252F%252Faffil.walmart.com%26items%3D${sku}%7C1%26upcs%3D)`
+                    console.log(`[time: ${new Date().toISOString()}, product: ${sku}, title: ${title}]`)
+                    for (let group of sites[site]) {
+                        await helper.postAmazon(url, title, sku, price, image, stock, offerid, groups[group], site, version, qt, links)
+                    }
+                    PRODUCTS[sku].sizes = 'In-Stock'
+                    database.query(`update ${table} set sizes='In-Stock' where sku='${sku}'`)
+                }
+            } else {
+                if (status !== "Out-of-Stock") {
+                    PRODUCTS[sku].sizes = 'Out-of-Stock'
+                    database.query(`update ${table} set sizes='Out-of-Stock' where sku='${sku}'`)
+                }
+            }
+        await helper.sleep(product.waittime);
+        monitor(sku);
+        return
+    } catch (e) {
+        if(e.message.includes('Cannot read')){
+            monitor(sku)
+            return
+        }
+        console.log(e)
+        monitor(sku)
+        return
+    }
+}
+helper.discordbot(CHANNEL, PRODUCTS, table, monitor, site)
--- test2.js
+++ test2.js
@@ -1,2 +1,21 @@
-var randomBoolean = Math.random() < 0.5
-console.log(randomBoolean)
+const fetch = require('node-fetch');
+const { v4 } = require('uuid');
+const randomUseragent = require('random-useragent');
+let sku = "650545386"
+const fs = require('fs');
+let site = `https://www.walmart.com/orchestra/pdp/graphql/ItemById/8d596c4780b45446d5ccf7c95e35e38080d857437182a6b1b4f3b964f9d4c298/ip/519253793?variables=%7B%22channel%22%3A%22WWW%22%2C%22pageType%22%3A%22ItemPageGlobal%22%2C%22tenant%22%3A%22WM_GLASS%22%2C%22version%22%3A%22v1%22%2C%22iId%22%3A%22519253793%22%2C%22layout%22%3A%5B%22itemDesktop%22%5D%2C%22tempo%22%3A%7B%22targeting%22%3A%22%257B%2522userState%2522%253A%2522loggedIn%2522%257D%22%2C%22params%22%3A%5B%7B%22key%22%3A%22expoVars%22%2C%22value%22%3A%22expoVariationValue%22%7D%2C%7B%22key%22%3A%22expoVars%22%2C%22value%22%3A%22expoVariationValue2%22%7D%5D%7D%2C%22p13N%22%3A%7B%22userClientInfo%22%3A%7B%22isZipLocated%22%3Atrue%2C%22deviceType%22%3A%22desktop%22%7D%2C%22userReqInfo%22%3A%7B%22refererContext%22%3A%7B%22source%22%3A%22itempage%22%7D%2C%22pageUrl%22%3A%22%2Fip%2FM-M-s-Snickers-Twix-Milk-Chocolate-Halloween-Candy-56-Ct-Bulk-Bag%2F519253793%22%7D%7D%2C%22p13nCls%22%3A%7B%22pageId%22%3A%22519253793%22%2C%22userClientInfo%22%3A%7B%22isZipLocated%22%3Atrue%7D%2C%22userReqInfo%22%3A%7B%22refererContext%22%3A%7B%22source%22%3A%22itempage%22%7D%7D%2C%22p13NCallType%22%3A%22ATF%22%7D%2C%22fBBAd%22%3Atrue%2C%22fSL%22%3Atrue%2C%22fIdml%22%3Atrue%2C%22fRev%22%3Atrue%2C%22fFit%22%3Atrue%2C%22fSeo%22%3Atrue%2C%22fP13%22%3Atrue%2C%22fAff%22%3Atrue%2C%22fMq%22%3Atrue%2C%22fGalAd%22%3Afalse%2C%22fSCar%22%3Atrue%2C%22fBB%22%3Atrue%2C%22fDis%22%3Atrue%2C%22eItIb%22%3Atrue%2C%22fIlc%22%3Afalse%2C%22includeLabelV1%22%3Afalse%7D`;
+async function test() {
+    let resp = await fetch("https://www.walmart.com/orchestra/pdp/graphql/ItemById/8d596c4780b45446d5ccf7c95e35e38080d857437182a6b1b4f3b964f9d4c298/ip/519253793?variables=%7B%22channel%22%3A%22WWW%22%2C%22pageType%22%3A%22ItemPageGlobal%22%2C%22tenant%22%3A%22WM_GLASS%22%2C%22version%22%3A%22v1%22%2C%22iId%22%3A%22519253793%22%2C%22layout%22%3A%5B%22itemDesktop%22%5D%2C%22tempo%22%3A%7B%22targeting%22%3A%22%257B%2522userState%2522%253A%2522loggedIn%2522%257D%22%2C%22params%22%3A%5B%7B%22key%22%3A%22expoVars%22%2C%22value%22%3A%22expoVariationValue%22%7D%2C%7B%22key%22%3A%22expoVars%22%2C%22value%22%3A%22expoVariationValue2%22%7D%5D%7D%2C%22p13N%22%3A%7B%22userClientInfo%22%3A%7B%22isZipLocated%22%3Atrue%2C%22deviceType%22%3A%22desktop%22%7D%2C%22userReqInfo%22%3A%7B%22refererContext%22%3A%7B%22source%22%3A%22itempage%22%7D%2C%22pageUrl%22%3A%22%2Fip%2FM-M-s-Snickers-Twix-Milk-Chocolate-Halloween-Candy-56-Ct-Bulk-Bag%2F519253793%22%7D%7D%2C%22p13nCls%22%3A%7B%22pageId%22%3A%22519253793%22%2C%22userClientInfo%22%3A%7B%22isZipLocated%22%3Atrue%7D%2C%22userReqInfo%22%3A%7B%22refererContext%22%3A%7B%22source%22%3A%22itempage%22%7D%7D%2C%22p13NCallType%22%3A%22ATF%22%7D%2C%22fBBAd%22%3Atrue%2C%22fSL%22%3Atrue%2C%22fIdml%22%3Atrue%2C%22fRev%22%3Atrue%2C%22fFit%22%3Atrue%2C%22fSeo%22%3Atrue%2C%22fP13%22%3Atrue%2C%22fAff%22%3Atrue%2C%22fMq%22%3Atrue%2C%22fGalAd%22%3Afalse%2C%22fSCar%22%3Atrue%2C%22fBB%22%3Atrue%2C%22fDis%22%3Atrue%2C%22eItIb%22%3Atrue%2C%22fIlc%22%3Afalse%2C%22includeLabelV1%22%3Afalse%7D", {
+        "headers": {
+          "accept": "application/json",
+          "Referer": "https://www.walmart.com/ip/M-M-s-Snickers-Twix-Milk-Chocolate-Halloween-Candy-56-Ct-Bulk-Bag/519253793",
+          "Referrer-Policy": "strict-origin-when-cross-origin"
+        },
+        "body": null,
+        "method": "GET"
+      });
+    console.log(resp.status)
+    let body = await resp.json();
+    console.log(body.data.product.name)
+}
+test()
