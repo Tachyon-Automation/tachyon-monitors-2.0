5f1606bf6064eb0639d23ef0dfc8153eec966bea
aggiekouv <aggiekouvaras@gmail.com>
2022-11-05 16:49:24 -0400

    start

--- aio.config.js
+++ aio.config.js
@@ -22,6 +22,13 @@ module.exports = {
             "out_file": "/dev/null"
         },
         {
+            "script": "./AIO Sites/crocs.js",
+            "watch": true,
+            "max_memory_restart": "1000M",
+            "error_file": "/dev/null",
+            "out_file": "/dev/null"
+        },
+        {
             "script": "./AIO Sites/finishline.js",
             "watch": true,
             "max_memory_restart": "1000M",
