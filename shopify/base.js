37c3e9389e0d07e3f3db40fcf4f34626a1f4798a
aggiekouv <aggiekouvaras@gmail.com>
2022-11-09 20:02:32 -0500

    shop new

--- shopify/base.js
+++ shopify/base.js
@@ -20,7 +20,9 @@ class ShopifyMonitor {
     async monitor() {
         this.monitorAntibot();
         this.monitorProducts("1", "250");
+        await helper.sleep(200)
         this.monitorProducts("1", "250");
+        await helper.sleep(200)
         this.monitorProducts("1", "250");
     }
 
