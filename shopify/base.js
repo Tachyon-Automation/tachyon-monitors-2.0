a72195639c29a5f009f10b945d7b4c026ae7d615
aggiekouv <aggiekouvaras@gmail.com>
2022-11-09 21:40:51 -0500

    shop new

--- shopify/base.js
+++ shopify/base.js
@@ -72,12 +72,12 @@ class ShopifyMonitor {
                     if (variant.available && !variants.includes(variant.id)) {
                         if (variant.inventory_quantity) {
                             variants.push(variant.id);
-                            sizes += `[${variant.title}](${this.WEBSITE}/cart/${variant.id}:1) | [QT](http://tachyonrobotics.com) (${variant.inventory_quantity})\n`
+                            sizes += `[${variant.option1}](${this.WEBSITE}/cart/${variant.id}:1) | [QT](http://tachyonrobotics.com) (${variant.inventory_quantity})\n`
                             price = variant.price;
                             stock += variant.inventory_quantity
                         } else {
                             variants.push(variant.id);
-                            sizes += `[${variant.title}](${this.WEBSITE}/cart/${variant.id}:1) | [QT](http://tachyonrobotics.com) (1+)\n`
+                            sizes += `[${variant.option1}](${this.WEBSITE}/cart/${variant.id}:1) | [QT](http://tachyonrobotics.com) (1+)\n`
                             price = variant.price;
                             stock++
                         }
