12d9a120a4d978342c4c8d65d432d297ec0a0050
aggiekouv <aggiekouvaras@gmail.com>
2022-11-09 21:57:33 -0500

    shop new

--- shopify/base.js
+++ shopify/base.js
@@ -19,7 +19,6 @@ class ShopifyMonitor {
         this.monitorAntibot();
         this.monitorProducts("1", "250");
         this.monitorProducts("1", "250");
-        this.monitorProducts("1", "250");
     }
 
     async monitorProducts(page, limit) {
