3d83c36930a539c897e04c1f852222b202d20608
aggiekouv <aggiekouvaras@gmail.com>
2022-11-09 22:39:43 -0500

    shop new

--- shopify/shopify6.js
+++ shopify/shopify6.js
@@ -20,6 +20,7 @@ monitors.push(new ShopifyMonitor("https://laborskateshop.com", 'NONE'))
 monitors.push(new ShopifyMonitor("https://stay-rooted.com", 'NONE'))
 monitors.push(new ShopifyMonitor("https://leencustoms.com", 'NONE'))
 monitors.push(new ShopifyMonitor("https://futurerfrnce.com", 'NONE'))
+monitors.push(new ShopifyMonitor("https://extrabutterny.com", 'NONE'))
 //
 for(let monitor of monitors) {
     monitor.monitor();
