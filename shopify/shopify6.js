ef01cf8601554f8757cf4395cf92ee2c7628670d
aggiekouv <aggiekouvaras@gmail.com>
2022-11-12 04:04:53 -0500

    start

--- shopify/shopify6.js
+++ shopify/shopify6.js
@@ -1,10 +1,9 @@
 let ShopifyMonitor = require('./base.js');
 let monitors = [];
 monitors.push(new ShopifyMonitor("https://plaskateboarding.com", 'NONE'))
-monitors.push(new ShopifyMonitor("https://www.tcg-stadium.com", 'NONE'))
 monitors.push(new ShopifyMonitor("https://thebettergeneration.com", 'NONE'))
 monitors.push(new ShopifyMonitor("https://shop.ccs.com", 'NONE'))
-monitors.push(new ShopifyMonitor("https://www.303boards.com", 'NONE'))
+monitors.push(new ShopifyMonitor("https://303boards.com", 'NONE'))
 monitors.push(new ShopifyMonitor("https://area51store.co.nz", 'NONE'))
 monitors.push(new ShopifyMonitor("https://twofeetundr.com", 'NONE'))
 monitors.push(new ShopifyMonitor("https://chalicecollectibles.com", 'NONE'))
@@ -14,7 +13,7 @@ monitors.push(new ShopifyMonitor("https://shopwss.com", 'NONE'))
 monitors.push(new ShopifyMonitor("https://ficegallery.com", 'NONE'))
 monitors.push(new ShopifyMonitor("https://ferraramarketinc.com", 'NONE'))
 monitors.push(new ShopifyMonitor("https://finalmouse.com", 'NONE'))
-monitors.push(new ShopifyMonitor("https://www.tcg-stadium.com", 'NONE'))
+monitors.push(new ShopifyMonitor("https://tcg-stadium.com", 'NONE'))
 monitors.push(new ShopifyMonitor("https://shopjustsports.com", 'NONE'))
 monitors.push(new ShopifyMonitor("https://laborskateshop.com", 'NONE'))
 monitors.push(new ShopifyMonitor("https://stay-rooted.com", 'NONE'))
@@ -34,6 +33,10 @@ monitors.push(new ShopifyMonitor("https://safari-zone.com", 'NONE'))
 monitors.push(new ShopifyMonitor("https://soleclassics.com", 'NONE'))
 monitors.push(new ShopifyMonitor("https://sneakerboxshop.ca", 'UNFILTEREDCA'))
 monitors.push(new ShopifyMonitor("https://drinkprime.uk", 'NONE'))
+monitors.push(new ShopifyMonitor("https://oqium.com", 'NONE'))
+monitors.push(new ShopifyMonitor("https://thenextdoor.fr", 'NONE'))
+monitors.push(new ShopifyMonitor("https://zulusgames.com", 'NONE'))
+//
 //
 for(let monitor of monitors) {
     monitor.monitor();
