357419915077a4bbbd95bef4ada9f9b9eb4619c5
aggiekouv <aggiekouvaras@gmail.com>
2022-11-14 22:51:15 -0500

    start

--- shopify/shopify7.js
+++ shopify/shopify7.js
@@ -22,6 +22,18 @@ monitors.push(new ShopifyMonitor("https://eflash.doverstreetmarket.com", 'DSMUK'
 monitors.push(new ShopifyMonitor("https://shop-us.doverstreetmarket.com", 'DSMUS'))
 monitors.push(new ShopifyMonitor("https://shop-sg.doverstreetmarket.com", 'DSMSG'))
 monitors.push(new ShopifyMonitor("https://shop-jp.doverstreetmarket.com", 'DSMJP'))
+monitors.push(new ShopifyMonitor("https://solefly.com", 'NONE'))
+monitors.push(new ShopifyMonitor("https://asphaltgold.com", 'NONE'))
+monitors.push(new ShopifyMonitor("https://millenniumshoes.com", 'NONE'))
+monitors.push(new ShopifyMonitor("https://shopwss.com", 'NONE'))
+monitors.push(new ShopifyMonitor("https://likelihood.myshopify.com", 'NONE'))
+monitors.push(new ShopifyMonitor("https://uptherestore.com", 'NONE'))
+monitors.push(new ShopifyMonitor("https://wishatl.com", 'NONE'))
+monitors.push(new ShopifyMonitor("https://www.addictmiami.com", 'NONE'))
+monitors.push(new ShopifyMonitor("https://drifthouse.com", 'NONE'))
+monitors.push(new ShopifyMonitor("https://store.sony.com.my", 'NONE'))
+monitors.push(new ShopifyMonitor("https://jetstore.com.mx", 'NONE'))
+monitors.push(new ShopifyMonitor("https://capusa.nyc", 'NONE'))
 //
 //
 for(let monitor of monitors) {
