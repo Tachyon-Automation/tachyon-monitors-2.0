59841e17eaca826973494fe580e7c22027370e3d
aggiekouv <aggiekouvaras@gmail.com>
2022-11-15 22:05:09 -0500

    starts

--- shopify/base.js
+++ shopify/base.js
@@ -18,9 +18,9 @@ class ShopifyMonitor {
 
     async monitor() {
         //this.monitorAntibot();
-        this.monitorProducts("1", "100", lastHash, products);
-        this.monitorProducts("2", "100", lastHash, products);
-        this.monitorProducts("3", "100", lastHash, products);
+        this.monitorProducts("1", "250", lastHash, products);
+        this.monitorProducts("2", "250", lastHash, products);
+        this.monitorProducts("3", "250", lastHash, products);
 
     }
 
