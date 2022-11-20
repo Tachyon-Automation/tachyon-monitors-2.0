4c2628600bcfa6d806944747e9e082982d1477cf
aggiekouv <aggiekouvaras@gmail.com>
2022-11-20 15:50:21 -0500

    start

--- hibbett.config.js
+++ hibbett.config.js
@@ -1,14 +1,7 @@
 module.exports = {
     "apps": [
         {
-            "script": "./AIO Sites/hibbett.js",
-            "watch": true,
-            "max_memory_restart": "1000M",
-            "error_file": "/dev/null",
-            "out_file": "/dev/null"
-        },
-        {
-            "script": "./AIO Sites/hibbett3.js",
+            "script": "./AIO Sites/hibbett5.js",
             "watch": true,
             "max_memory_restart": "1000M",
             "error_file": "/dev/null",
